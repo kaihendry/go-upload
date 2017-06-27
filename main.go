56cc6d9922c8e4767ed9e8c13fec16ea49acfa8d
Kai Hendry <hendry@webconverger.com>
2017-06-28 07:09:46 +0800

    Empty files

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"runtime/pprof"
 )
 
 func upload(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +43,18 @@ func upload(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
